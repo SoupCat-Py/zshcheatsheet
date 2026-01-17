f69ad4f6fe2f8bcdf69e1a0a8b3cf6da0e6a9fb6
SoupCat <soupcat.py@gmail.com>
2026-01-16 22:53:23 -0700

    added some stuff

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -6,22 +6,57 @@ export default function CardContainer() {
   return (
     <div className={`d-grid g1 ${styles.container}`}>
       <Card
-        command="mv FILE DESTINATION_PATH"
+        command="mv FILE DESTINATION"
         description={
           <>
-            This example has many lines of text
+            Move <CodeSnippet i="FILE" /> to <CodeSnippet i="DESTINATION" />
             <br />
-            <ul>
-              <li>It takes JSX instead of a string</li>
-              <li>I think it can take strings too</li>
-              <li>
-                Explain what <CodeSnippet i="args" /> does
-              </li>
-              <li>
-                move <CodeSnippet i="FILE" /> to{" "}
-                <CodeSnippet i="DESTINATION_PATH" />
-              </li>
-            </ul>
+            Use the same directory in <CodeSnippet i="FILE" /> and{" "}
+            <CodeSnippet i="DESTINATION" /> to rename <CodeSnippet i="FILE" />
+            <br />
+            <br />
+            Examples: <br />
+            move <CodeSnippet i="mv ~/file.txt ~/dir/file.txt" />
+            <br />
+            rename <CodeSnippet i="mv ~/file.txt ~/file2.txt" />
+          </>
+        }
+      />
+      <Card
+        command="cp FILE DESTINATION"
+        description={
+          <>
+            Copy <CodeSnippet i="FILE" /> to <CodeSnippet i="DESTINATION" />
+          </>
+        }
+      />
+      <Card
+        command="rm [-rfvi] FILE"
+        description={
+          <>
+            Delete <CodeSnippet i="FILE" />
+            <br />
+            <CodeSnippet i="-r" /> deletes recursively (for non-empty
+            directories)
+            <br />
+            <CodeSnippet i="-f" /> force deletion
+            <br />
+            <CodeSnippet i="-v" /> verbose (print out files as they get deleted)
+            <br />
+            <CodeSnippet i="-i" /> request confirmation before deleting each
+            file (must type "y" or "n")
+            <br />
+            Note that <CodeSnippet i="-i" /> overrides <CodeSnippet i="-f" />
+          </>
+        }
+      />
+      <Card
+        command="rmdir DIR"
+        description={
+          <>
+            Delete <CodeSnippet i="DIR" />
+            <br />
+            Note that <CodeSnippet i="DIR" /> must be <strong>empty</strong>.
           </>
         }
       />
