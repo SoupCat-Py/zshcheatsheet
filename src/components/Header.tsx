591b315b2004d20e616667963f8d7fbd7011f681
SoupCat <soupcat.py@gmail.com>
2026-01-16 17:20:32 -0700

    reformat layout for a more robust centering... i think

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,56 +1,67 @@
-import styles from "../styles/Header.module.css";
-
 export default function Header() {
   return (
-    <div className="pos-rel d-flex flex-row bg-norm justify-between align-center p2 r1">
-
-      <a
-        className="btn bg-btn-dark color-text p05 r05 g025 nodeco d-flex flex-row align-center justify-center"
-        href="https://react.dev"
-        target="_blank"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="3"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          className="size4"
+    <div className="d-flex flex-row bg-norm align-center p2 r1">
+      <div className="d-flex flex-row f1 justify-start">
+        <a
+          className="btn bg-btn-dark color-text p05 r05 g025 nodeco d-flex flex-row align-center justify-center"
+          href="https://react.dev"
+          target="_blank"
         >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
-        </svg>
-        <span className="size4">project repo</span>
-      </a>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="3"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="size4"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
+          </svg>
+          <span className="size4">project repo</span>
+        </a>
+      </div>
 
-      <h1 className={`pos-abs size1 color-text margin0 padding0 ${styles.h1}`}>
-        Header
-      </h1>
+      <div className="d-flex flex-row f1 justify-center">
+        <h1 className="size1 color-text margin0 padding0">
+          Header
+        </h1>
+      </div>
 
-      <a
-        className="btn bg-btn-dark color-text p05 r05 g025 nodeco d-flex flex-row align-center justify-center"
-        href="https://vite.dev"
-        target="_blank"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="3"
-          stroke="currentColor"
-          className="size4"
+      <div className="d-flex flex-row f1 justify-end">
+        <a
+          className="btn bg-btn-dark color-text p05 r05 g025 nodeco d-flex flex-row align-center justify-center"
+          href="https://vite.dev"
+          target="_blank"
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
-          />
-        </svg>
-      </a>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="3"
+            stroke="currentColor"
+            className="size4"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
+            />
+          </svg>
+        </a>
+      </div>
     </div>
   );
 }
+
+// quick idea
+// make three sections inside the header
+// each one has a flex of 1 (three equal parts)
+// part 1: justify left
+// part 2: justify center
+// part 3: justify right
+// this should center the header while putting the buttons at their respective edges
