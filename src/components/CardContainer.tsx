c4853e7893f10c4b4ac74e7217a9c129d2a78441
SoupCat <soupcat.py@gmail.com>
2026-01-16 20:37:09 -0700

    trying out display-grid for this
    idk if i'll ust it in the end bc of headers

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -1,11 +1,13 @@
 import Card from "./Card";
 import CodeSnippet from "./CodeSnippet";
+import styles from "../styles/CardContainer.module.css";
 
 export default function CardContainer() {
   return (
-    <div className="d-flex flex-col g1">
+    <div className={`d-grid g1 ${styles.container}`}>
+
       <Card
-        command="mv ORIGINAL_PATH DESTINATION_PATH"
+        command="mv FILE DESTINATION_PATH"
         description={
           <>
             This example has many lines of text
@@ -13,12 +15,18 @@ export default function CardContainer() {
             <ul>
               <li>It takes JSX instead of a string</li>
               <li>I think it can take strings too</li>
-              <li>Explain what <CodeSnippet i="args"/> does</li>
-              <li>move <CodeSnippet i="ORIGINAL_PATH"/> to <CodeSnippet i="DESTINATION_PATH"/></li>
+              <li>
+                Explain what <CodeSnippet i="args" /> does
+              </li>
+              <li>
+                move <CodeSnippet i="FILE" /> to{" "}
+                <CodeSnippet i="DESTINATION_PATH" />
+              </li>
             </ul>
           </>
         }
       />
+
     </div>
   );
 }
