0fd112e4be769b48c035a7633004bdaf1947dc3b
SoupCat <soupcat.py@gmail.com>
2026-01-29 11:42:39 -0700

    added a bunch of testing cards and functionality to sidebar

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,10 +1,11 @@
 export default function Aside() {
     return (
-        <div className="d-flex flex-col bg-norm r1 p1 color-text">
-            <a>link</a>
-            <a>link</a>
-            <a>link</a>
-            <a>link</a>
+        <div className="d-flex flex-col bg-norm r1 p2 g05 color-text">
+            <a className="color-text size4" href="#section1">Navigate & Display</a>
+            <a className="color-text size4" href="#section2">Move, Copy, Delete</a>
+            <a className="color-text size4" href="#section3">Dir & File Creation</a>
+            <a className="color-text size4" href="#section4">Combining Commands</a>
+            <a className="color-text size4" href="#section5">Miscellaneous</a>
         </div>
     )
 }
--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -4,26 +4,46 @@ import styles from "../styles/CardContainer.module.css";
 
 export default function CardContainer() {
   return (
-    <section className="d-flex flex-col g1 p1">
-
-      <h2 className={`size2 color-primary margin0 padding0 ${styles.subheader} ${styles.first}`}>
+    <section className={`d-flex flex-col d1 r1 ${styles.container}`}>
+      <h2
+        className={`size2 color-primary margin0 padding0 ${styles.subheader} ${styles.first}`}
+        id="section1"
+      >
         Section Header
       </h2>
 
-      <div className={`d-grid g1 ${styles.section}`}>
-        <Card command="hello" description="test"/>
-        <Card command="world" description="test"/>
+      <div className={`d-grid g2 ${styles.section}`}>
+        <Card command="hello" description="test" />
+        <Card command="world" description="test" />
+        <Card command="hello" description="test" />
+        <Card command="world" description="test" />
+        <Card command="hello" description="test" />
+        <Card command="world" description="test" />
+        <Card command="hello" description="test" />
+        <Card command="world" description="test" />
+        <Card command="hello" description="test" />
+        <Card command="world" description="test" />
+        <Card command="hello" description="test" />
+        <Card command="world" description="test" />
+        <Card command="hello" description="test" />
+        <Card command="world" description="test" />
+        <Card command="hello" description="test" />
+        <Card command="world" description="test" />
+        <Card command="hello" description="test" />
+        <Card command="world" description="test" />
       </div>
 
-      <h2 className={`size2 color-primary margin0 padding0 ${styles.subheader}`}>
+      <h2
+        className={`size2 color-primary margin0 padding0 ${styles.subheader}`}
+        id="section2"
+      >
         Section Header
       </h2>
 
-      <div className={`d-grid g1 ${styles.section}`}>
-        <Card command="hello" description="test"/>
-        <Card command="world" description="test"/>
+      <div className={`d-grid g2 ${styles.section}`}>
+        <Card command="hello" description="test" />
+        <Card command="world" description="test" />
       </div>
-
     </section>
   );
 }
