82506272565bd449eb931b15d4ba1bb8c557c6bf
SoupCat <soupcat.py@gmail.com>
2026-01-30 10:27:34 -0700

    tweaked the header a bit
    - removed gap in github button
    - replaced moon icon with a sun
    - adjusted stroke widths in both SVGs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ export default function Header() {
     <div className="d-flex flex-row bg-norm align-center p1 r1">
       <div className="d-flex flex-row f1 justify-start">
         <a
-          className="btn bg-btn-dark color-text p05 r05 g025 nodeco d-flex flex-row align-center justify-center"
+          className="btn bg-btn-dark color-text p05 r05 nodeco d-flex flex-row align-center justify-center"
           href="https://github.com/soupcat-py/codesheet2"
           target="_blank"
         >
@@ -14,7 +14,7 @@ export default function Header() {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="3"
+            stroke-width="2.5"
             stroke-linecap="round"
             stroke-linejoin="round"
             className="size4"
@@ -42,14 +42,14 @@ export default function Header() {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            strokeWidth="3"
+            strokeWidth="2"
             stroke="currentColor"
-            className="size4"
+            className="size3"
           >
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
-              d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
+              d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
             />
           </svg>
         </a>
