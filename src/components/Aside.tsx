ab203ff87b1b20871e71d39c040c572d06ae8afa
SoupCat <soupcat.py@gmail.com>
2026-01-31 15:55:37 -0700

    reordered stuff & added JSX hover effects

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,23 +1,39 @@
+import { useState } from "react";
+
+type asideLinkProp = {
+  section: string;
+  label: string;
+};
+
+const AsideLink = ({ section, label }: asideLinkProp) => {
+  // init state
+  const [isHovered, setHovered] = useState(false);
+
+  // func to toggle state
+  const toggleHovered = () => setHovered(!isHovered);
+
+  return (
+    <a
+      className={`size4 ${!isHovered ? "color-text nodeco" : "color-primary"}`}
+      href={`#section${section}`}
+      onMouseEnter={toggleHovered}
+      onMouseLeave={toggleHovered}
+    >
+      {label}
+    </a>
+  );
+};
+
 export default function Aside() {
   return (
     <div className="d-flex flex-col bg-norm r1 p2 g1">
       <h3 className="h3 color-primary size3 padding0 margin0">Sections</h3>
       <div className="d-flex flex-col bg-norm g05">
-        <a className="color-text size4" href="#section1">
-          Navigate & Display
-        </a>
-        <a className="color-text size4" href="#section2">
-          Move, Copy, Delete
-        </a>
-        <a className="color-text size4" href="#section3">
-          Dir & File Creation
-        </a>
-        <a className="color-text size4" href="#section4">
-          Combining Commands
-        </a>
-        <a className="color-text size4" href="#section5">
-          Miscellaneous
-        </a>
+        <AsideLink section="1" label="Navigate & Display" />
+        <AsideLink section="2" label="Dir & File Creation" />
+        <AsideLink section="3" label="Move, Copy, Delete" />
+        <AsideLink section="4" label="Combining Commands" />
+        <AsideLink section="5" label="Miscellaneous" />
       </div>
     </div>
   );
