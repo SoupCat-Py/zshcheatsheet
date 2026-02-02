02ed53d2b22712074188d8f36a2e0da10c8afdd9
SoupCat <soupcat.py@gmail.com>
2026-02-02 12:45:56 -0700

    added functionality to theme toggle

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,57 @@
+import { useState } from "react";
+
+// get :root
+const root = document.querySelector(":root");
+
 export default function Header() {
+  // moon svg
+  const moon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size3"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
+      />
+    </svg>
+  );
+
+  // sun svg
+  const sun = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="2"
+      stroke="currentColor"
+      className="size3"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+      />
+    </svg>
+  );
+
+  // init button state
+  const [isDarkMode, setDarkMode] = useState(true);
+
+  // func to toggle dark mode
+  const toggleIcon = () => {
+    // change button icon
+    setDarkMode(!isDarkMode);
+    // change :root class
+    root?.classList.add(isDarkMode ? "light" : "dark");
+    root?.classList.remove(isDarkMode ? "dark" : "light");
+  };
+
   return (
     <div className="d-flex flex-row bg-norm align-center p1 r1">
       <div className="d-flex flex-row f1 justify-start">
@@ -33,26 +86,12 @@ export default function Header() {
       </div>
 
       <div className="d-flex flex-row f1 justify-end">
-        <a
-          className="btn bg-btn-dark color-text p05 r05 g025 nodeco d-flex flex-row align-center justify-center"
-          href="https://vite.dev"
-          target="_blank"
+        <button
+          className="btn bg-btn-dark color-text p05 r05 g025 d-flex flex-row align-center justify-center"
+          onClick={toggleIcon}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="2"
-            stroke="currentColor"
-            className="size3"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
-            />
-          </svg>
-        </a>
+          {isDarkMode ? sun : moon}
+        </button>
       </div>
     </div>
   );
