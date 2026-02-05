0fa2590f2b675e382c295f568c328ec6f4020342
SoupCat <soupcat.py@gmail.com>
2026-02-05 11:02:24 -0700

    maybe this will help with aria idk

--- src/components/BodyContainer.tsx
+++ src/components/BodyContainer.tsx
@@ -5,9 +5,9 @@ import styles from "../styles/BodyContainer.module.css";
 export default function BodyContainer() {
   return (
     <div className={`d-grid g1 ${styles.container}`}>
-      <div className={styles.aside}>
+      <aside className={styles.aside}>
         <Aside />
-      </div>
+      </aside>
       <CardContainer />
     </div>
   );
